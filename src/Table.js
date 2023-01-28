bd7d81fac0e4eb35f2dacab50dda486d82592c5d
memerchik <yaro3245@gmail.com>
2023-01-28 01:03:50 +0100

    Finished

--- src/Table.js
+++ src/Table.js
@@ -8,26 +8,212 @@ function Table({play}) {
     const [key, setKey] = useState(1) // render
 
     function countNeighbours(cellRow, cellColumn){
-        let neighboursCount=0;
+        let neighboursArr=[];
 
         //default case
         if(cellRow != 0 && cellColumn != 0 && cellRow != matrix.length - 1 && cellColumn!=matrix.length-1){
             for(let i=cellRow-1;i<cellRow+2;i++){
                 if(matrix[i][cellColumn-1]==1){
-                    neighboursCount++;
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn-1
+                    })
                 }
                 if(matrix[i][cellColumn+1]==1){
-                    neighboursCount++;
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn+1
+                    })
                 }
                 if(matrix[i][cellColumn]==1&&i!=cellRow){
-                    neighboursCount++;
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn
+                    })
                 }
             }
-            return neighboursCount
+            return neighboursArr
         }
 
+        //if cell is in the first column, but not in the corner
+        if(cellRow != 0 && cellRow!=matrix.length-1 && cellColumn == 0){
+            for(let i=cellRow-1;i<cellRow+2;i++){
+                if(matrix[i][cellColumn+1]==1){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn+1
+                    })
+                }
+                if(matrix[i][cellColumn]==1&&i!=cellRow){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn
+                    })
+                }
+            }
+            return neighboursArr
+        }
 
-        
+        //if cell is in the last column, but not in the corner
+        if(cellRow != 0 && cellRow!=matrix.length-1 && cellColumn == matrix.length-1){
+            for(let i=cellRow-1;i<cellRow+2;i++){
+                if(matrix[i][cellColumn-1]==1){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn-1
+                    })
+                }
+                if(matrix[i][cellColumn]==1&&i!=cellRow){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn
+                    })
+                }
+            }
+            return neighboursArr
+        }
+
+        //if cell is in the first row, but not in the corner
+        if(cellRow == 0 && cellColumn != 0 && cellColumn!=matrix.length-1){
+            for(let i=cellRow;i<cellRow+2;i++){
+                if(matrix[i][cellColumn-1]==1){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn-1
+                    })
+                }
+                if(matrix[i][cellColumn+1]==1){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn+1
+                    })
+                }
+                if(matrix[i][cellColumn]==1&&i!=cellRow){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn
+                    })
+                }
+            }
+            return neighboursArr
+        }
+
+        //if cell is in the last row, but not in the corner
+        if(cellRow == matrix.length-1 && cellColumn != 0 && cellColumn!=matrix.length-1){
+            for(let i=cellRow-1;i<cellRow+1;i++){
+                if(matrix[i][cellColumn-1]==1){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn-1
+                    })
+                }
+                if(matrix[i][cellColumn+1]==1){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn+1
+                    })
+                }
+                if(matrix[i][cellColumn]==1&&i!=cellRow){
+                    neighboursArr.push({
+                        row: i,
+                        column: cellColumn
+                    })
+                }
+            }
+            return neighboursArr
+        }
+
+        //if cell is in the top left corner
+        if(cellRow == 0 && cellColumn == 0){
+            if(matrix[cellRow+1][cellColumn]==1){
+                neighboursArr.push({
+                    row: cellRow+1,
+                    column: cellColumn
+                })
+            }
+            if(matrix[cellRow+1][cellColumn+1]==1){
+                neighboursArr.push({
+                    row: cellRow+1,
+                    column: cellColumn+1
+                })
+            }
+            if(matrix[cellRow][cellColumn+1]==1){
+                neighboursArr.push({
+                    row: cellRow,
+                    column: cellColumn+1
+                })
+            }
+            return neighboursArr
+        }
+
+        //if cell is in the top right corner
+        if(cellRow == 0 && cellColumn == matrix.length-1){
+            if(matrix[cellRow+1][cellColumn]==1){
+                neighboursArr.push({
+                    row: cellRow+1,
+                    column: cellColumn
+                })
+            }
+            if(matrix[cellRow+1][cellColumn-1]==1){
+                neighboursArr.push({
+                    row: cellRow+1,
+                    column: cellColumn-1
+                })
+            }
+            if(matrix[cellRow][cellColumn-1]==1){
+                neighboursArr.push({
+                    row: cellRow,
+                    column: cellColumn-1
+                })
+            }
+            return neighboursArr
+        }
+
+        //if cell is in the bottom right corner
+        if(cellRow == matrix.length-1 && cellColumn == matrix.length-1){
+            if(matrix[cellRow-1][cellColumn]==1){
+                neighboursArr.push({
+                    row: cellRow-1,
+                    column: cellColumn
+                })
+            }
+            if(matrix[cellRow-1][cellColumn-1]==1){
+                neighboursArr.push({
+                    row: cellRow-1,
+                    column: cellColumn-1
+                })
+            }
+            if(matrix[cellRow][cellColumn-1]==1){
+                neighboursArr.push({
+                    row: cellRow,
+                    column: cellColumn-1
+                })
+            }
+            return neighboursArr
+        }
+
+        //if cell is in the bottom left corner
+        if(cellRow == matrix.length-1 && cellColumn == 0){
+            if(matrix[cellRow-1][cellColumn]==1){
+                neighboursArr.push({
+                    row: cellRow-1,
+                    column: cellColumn
+                })
+            }
+            if(matrix[cellRow-1][cellColumn+1]==1){
+                neighboursArr.push({
+                    row: cellRow-1,
+                    column: cellColumn+1
+                })
+            }
+            if(matrix[cellRow][cellColumn+1]==1){
+                neighboursArr.push({
+                    row: cellRow,
+                    column: cellColumn+1
+                })
+            }
+            return neighboursArr
+        }      
     }
     
 
@@ -40,25 +226,7 @@ function Table({play}) {
         if(action=="setAlive"){
             let matrix2 = matrix;
             matrix2[cellRow][cellColumn]=1
-            changeMatrix(matrix2)
-            console.log(countNeighbours(cellRow, cellColumn))
-        }
-        if(action=="setDead"){
-            let matrix2 = matrix;
-            matrix2[cellRow][cellColumn]=0
-            changeMatrix(matrix2)
-        }
-        setKey((key)=>key+1) // render
-    }
-
-    function changeCell(cellRow, cellColumn, action){
-        if(play!=true){
-            return
-        }
-        //console.log("row:",cellRow, "column:", cellColumn, "action", action)
-        if(action=="setAlive"){
-            let matrix2 = matrix;
-            matrix2[cellRow][cellColumn]=1
+            console.log(countNeighbours(cellRow, cellColumn).length)
             changeMatrix(matrix2)
         }
         if(action=="setDead"){
@@ -71,9 +239,9 @@ function Table({play}) {
 
     //Play/Stop switch
     useEffect(() => {
+        setKey((key)=>key+1)
         const interval = setInterval(() => {
-            //function call here
-            console.log('This will be called every 2 seconds');
+            processMatrix(matrix)
         }, 500);
         
         if(play==true){
@@ -83,28 +251,76 @@ function Table({play}) {
             clearInterval(interval);
         }
         
-    }, [play]);
+    }, [play, matrix]);
 
     //function
 
-    function processMatrix(){
-        let newMatrix = matrix
-
-        
-
-        
-
+    function processMatrix(matrix){
+        let newMatrix = Array(18).fill(null).map(()=>Array(18).fill(0))
         matrix.map((matrixRow, ind1)=>{
-            matrixRow.map((thisCell, ind2)=>{
-                //Checks if cell has to die
-
-                //check 1
-
-                
+            matrixRow.map((filled, ind2)=>{
+                newMatrix[ind1][ind2]=matrix[ind1][ind2]
             })
         })
+        let emptyCells = []
+        let filledCells = []
+
         
 
+        
+        //sorting cells
+        matrix.map((matrixRow, ind1)=>{
+            matrixRow.map((filled, ind2)=>{
+                let neighbours = countNeighbours(ind1, ind2).length
+                //filtering
+                if(filled==0 && neighbours!=0){
+                    emptyCells.push({
+                        row: ind1,
+                        column: ind2
+                    })
+                }
+                else if(filled==1){
+                    filledCells.push({
+                        row: ind1,
+                        column: ind2
+                    })
+                }
+        
+            })
+        })
+
+        console.log("empty", emptyCells)
+        console.log("filled", filledCells)
+
+        //Check empty cells
+        emptyCells.map((thisCell, ind)=>{
+            let neighbours = countNeighbours(thisCell.row, thisCell.column)
+            if(neighbours.length==3){
+                neighbours.map((neighbour)=>{
+                    newMatrix[neighbour.row][neighbour.column]=0
+                })
+                newMatrix[thisCell.row][thisCell.column]=1
+            }
+        })
+        //check filled cells
+        filledCells.map((thisCell)=>{
+            let neighbours = countNeighbours(thisCell.row, thisCell.column)
+
+            //if cell has 0 or 1 neighbour
+            if(neighbours.length==0||neighbours.length==1){
+                newMatrix[thisCell.row][thisCell.column]=0
+            }
+
+            //if cell has 4 or more neighbours
+            if(neighbours.length>=4){
+                newMatrix[thisCell.row][thisCell.column]=0
+            }
+
+            if(neighbours.length==2||neighbours.length==3){
+                newMatrix[thisCell.row][thisCell.column]=1
+            }
+        })
+        changeMatrix(newMatrix)
     }
 
     return (
