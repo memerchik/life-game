77b5403a725dfda4d34af8d429c9451b73958eec
Yaroslav <yaro3245@gmail.com>
2023-03-07 09:50:31 +0100

    login register start

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios'
+
 
 function Login() {
+    const [usernameReg, setUsernameReg] = useState()
+    const [passwordReg, setPasswordReg] = useState()
+    const [usernameLog, setUsernameLog] = useState()
+    const [passwordLog, setPasswordLog] = useState()
+
+    const register = ()=>{
+        Axios.post("http://localhost:3001/register",
+        {
+            username: usernameReg,
+            password: passwordReg
+        }).then((res)=>{
+            console.log(res)
+        })
+    }
+
+    const login = ()=>{
+        Axios.post("http://localhost:3001/login",
+        {
+            username: usernameLog,
+            password: passwordLog
+        }).then((res)=>{
+            console.log(res)
+        })
+    }
+
     return (
         <div className='LoginPage'>
             <div className='Registration'>
-
+                <input type="text" id="regUsername" onChange={(e)=>{setUsernameReg(e.target.value)}}/>
+                <input type="text" id="regPassword" onChange={(e)=>{setPasswordReg(e.target.value)}}/>
+                <button onClick={register}>Register</button>
             </div>
             <div className='Login'>
-
+                <input type="text" id="logUsername" onChange={(e)=>{setUsernameLog(e.target.value)}}/>
+                <input type="text" id="logPassword" onChange={(e)=>{setPasswordLog(e.target.value)}}/>
+                <button onClick={login}>Login</button>
             </div>
         </div>
     )
--- server/index.js
+++ server/index.js
@@ -1,13 +1,52 @@
-import { Express } from "express";
+const express = require("express");
 const mysql = require("mysql");
+const cors = require("cors");
 
-const app = Express()
+const app = express();
 
-app.use(Express.json())
+const PORT = 3001
+
+app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: "",
-    database: ""
+    database: "logindb"
+})
+
+app.post('/register', (req, res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    db.query("INSERT INTO users (username, password) VALUES (?,?)", [username, password], (err, result)=>{
+        if(err){
+            res.send({err:err})
+        }
+        else{
+            res.send("success")
+        }
+    })
+})
+
+app.post("/login", (req, res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    db.query("SELECT * FROM users WHERE username=? AND password=?", [username, password], (err, result)=>{
+        if(err){
+            console.log(err)
+        }
+        else if(result){
+            console.log(result)
+        }
+        else{
+            res.send({message: "wrong username password combination"})
+        }
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log("server running on port "+PORT)
 })
